bba0397f09ec3f64e6f24d63bebd721f46c41d59
Sergii Pylypenko <x.pelya.x@gmail.com>
2015-07-27 21:00:03 +0300

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"1617","airdate":"1438192800","name_switch":"off","DJ":"Boz","showname":"BBB","blurb":"It's the Taint of the Week\u2122 again, so it's time for Boz to utter complete bollocks in between your requests. All aboard the Train-Wreck, WOO WOOH!","nice_showname":"<strong><big>Boz<\/big><\/strong><br \/>BBB","nice_airdate":"Wednesday 29th July, 2015<br \/>at 20:00 USRT"}]}
