fee687a8df4c98a455941af69d616bd625076ce4
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-01-19 18:00:02 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"1708","airdate":"1453402800","name_switch":"off","DJ":"KraxxUltima","showname":"The Ultimate Crack","blurb":"Hello everybody! How about another show?  Request some Commodore tunes ,remixed or original :)  Let's have some fun!","nice_showname":"<strong><big>KraxxUltima<\/big><\/strong><br \/>The Ultimate Crack","nice_airdate":"Thursday 21st January, 2016<br \/>at 20:00 USRT"}]}
