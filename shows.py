579bae62c8ba8c8560a72d68e5643e84e7dce60e
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-09-12 21:00:02 +0300

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"1801","airdate":"1473962400","name_switch":"off","DJ":"KraxxUltima","showname":"The Uncracked Track","blurb":"Another week means more tracks. So tune in and make your requests for Commodore music and similar audio bliss.   Let's have a good time to enhance the week =)\n","nice_showname":"<strong><big>KraxxUltima<\/big><\/strong><br \/>The Uncracked Track","nice_airdate":"Thursday 15th September, 2016<br \/>at 20:00 USRT"}]}
