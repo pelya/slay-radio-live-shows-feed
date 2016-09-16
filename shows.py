7ef44c0444db1d1da1cd1d32084b9a7a06178b68
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-09-16 21:00:02 +0300

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[{"show_ID":"1802","airdate":"1474048800","name_switch":"off","DJ":"Gurt","showname":"Radio Awesome","blurb":"Shooooooooooooooooooooowwwwwwwwwwwwwwwwwwwwww!!!!!!!!!!","nice_showname":"<strong><big>Gurt<\/big><\/strong><br \/>Radio Awesome","nice_airdate":"Friday 16th September, 2016<br \/>at 20:00 USRT"}]}
+{"error":0,"message":"","data":[]}
