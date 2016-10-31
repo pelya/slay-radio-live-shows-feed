2f61256bbfb02daba62019a3d8bd37d03d8ef456
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-10-31 14:00:02 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"1820","airdate":"1478113200","name_switch":"off","DJ":"Bozzy and the Bint","showname":"BBKB","blurb":"It's the Taint of the Week\u2122 again, so it's time for Boz to utter complete bollocks in between your requests. Thankfully, Kate is here to translate some of it into English. All aboard the Train-Wreck, WOO WOOH!","nice_showname":"<strong><big>Bozzy and the Bint<\/big><\/strong><br \/>BBKB","nice_airdate":"Wednesday 2nd November, 2016<br \/>at 20:00 USRT"}]}
