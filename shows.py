8c2e5341e610a3d56b1cd83693cdb52a58caf7f6
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-05-22 13:00:02 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"1915","airdate":"1495648800","name_switch":"off","DJ":"Boz","showname":"BBB","blurb":"Boz is flying solo again, so let's have a BBB! Talking utter bollocks in between your requests. All aboard the Train-Wreck, WOO WOOH!","nice_showname":"<strong><big>Boz<\/big><\/strong><br \/>BBB","nice_airdate":"Wednesday 24th May, 2017<br \/>at 20:00 USRT"}]}
