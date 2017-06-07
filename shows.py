3c923eb575c1fd0309e6378e853f06ce78d76ece
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-06-07 20:00:01 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[{"show_ID":"1922","airdate":"1496858400","name_switch":"off","DJ":"Bozzy and the Bint","showname":"BBKB","blurb":"SHE'S BACK! And just in time because Boz really wasn't grasping the English language very well at all. So join a refreshed Kate and Boz for another load of bollocks in between your requests. All aboard the Train-Wreck, WOO WOOH!","nice_showname":"<strong><big>Bozzy and the Bint<\/big><\/strong><br \/>BBKB","nice_airdate":"Wednesday 7th June, 2017<br \/>at 20:00 USRT"}]}
+{"error":0,"message":"","data":[]}
