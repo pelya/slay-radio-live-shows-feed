d785984659dd3ee31b2f11cd3ea69c9544b5eb5f
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-07-08 20:00:01 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[{"show_ID":"1936","airdate":"1499536800","name_switch":"off","DJ":"Gohanks","showname":"Retro For Sure","blurb":"The long awaited return is finally approaching. Bear with me for a while listening to great C64 tunes and rambling on about retro stuff. Might be a bumpy ride, getting back in the show saddle and re-learning stuff...","nice_showname":"<strong><big>Gohanks<\/big><\/strong><br \/>Retro For Sure","nice_airdate":"Saturday 8th July, 2017<br \/>at 20:00 USRT"}]}
+{"error":0,"message":"","data":[]}
