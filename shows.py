e1cda026d7e8679bccecb35ba3c1cb9cdb56e5e5
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-07-20 20:00:01 +0200

    Updated feeds

--- shows.py
+++ shows.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[{"show_ID":"1942","airdate":"1500573600","name_switch":"off","DJ":"KraxxUltima","showname":"The Uncracked Tracks","blurb":"Once more unto the breach, dear friends!(because otherwise we'd be on the beach catching some rays ;)  Request the usual (or unusual) Commodore 64\/Amiga tunes and let's have a good time =)","nice_showname":"<strong><big>KraxxUltima<\/big><\/strong><br \/>The Uncracked Tracks","nice_airdate":"Thursday 20th July, 2017<br \/>at 20:00 USRT"}]}
+{"error":0,"message":"","data":[]}
