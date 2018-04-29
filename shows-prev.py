f07c6d9c9430d0e2bc603838af5b879e09b548e7
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-04-29 23:00:02 +0300

    Updated feeds

--- shows-prev.py
+++ shows-prev.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"2057","airdate":"1525284000","name_switch":"off","DJ":"Boz","showname":"BBB","blurb":"It's time for Boz to transmit the final regular BBB and the penultimate BBKB, where he'll utter complete bollocks in between your requests. All aboard the Train-Wreck, WOO WOOH!","nice_showname":"<strong><big>Boz<\/big><\/strong><br \/>BBB","nice_airdate":"Wednesday 2nd May, 2018<br \/>at 20:00 USRT"}]}
