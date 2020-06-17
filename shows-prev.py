2a78015dbe31d22ef308710312484e95a6f004e3
Sergii Pylypenko <x.pelya.x@gmail.com>
2020-06-18 02:00:02 +0300

    Updated feeds

--- shows-prev.py
+++ shows-prev.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[]}
+{"error":0,"message":"","data":[{"show_ID":"2216","airdate":"1592503200","name_switch":"off","DJ":"ziona and Slaygon","showname":"Random thoughts","blurb":"Hi Everyone,\n\nYes, we are back and yes, it's Thursday. This time we just go totally random. Share your random thoughts and requests with us and we are happy to play them randomly! \n\nJoin us and stay random! ","nice_showname":"<strong><big>ziona and Slaygon<\/big><\/strong><br \/>Random thoughts","nice_airdate":"Thursday 18th June, 2020<br \/>at 20:00 USRT"}]}
