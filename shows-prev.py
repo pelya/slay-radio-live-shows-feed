810cdf25f514b5210b28c4f6f8fe91058405fc08
Sergii Pylypenko <x.pelya.x@gmail.com>
2020-09-17 22:00:01 +0300

    Updated feeds

--- shows-prev.py
+++ shows-prev.py
@@ -1,1 +1,1 @@
-{"error":0,"message":"","data":[{"show_ID":"2230","airdate":"1600365600","name_switch":"on","DJ":"Boz and Slaygon","showname":"Baffling Bonkers Broadcast","blurb":"Bona fide banter between buds. Better be baffled before bewildered. Boz & bud begin before 20:01.\n\nBe bedazzled by beholding the broadcast!","nice_showname":"<strong>Baffling Bonkers Broadcast<\/strong> with Boz and Slaygon","nice_airdate":"Thursday 17th September, 2020<br \/>at 20:00 USRT"}]}
+{"error":0,"message":"","data":[]}
